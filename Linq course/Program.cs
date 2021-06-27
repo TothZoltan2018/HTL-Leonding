81635c0d4434b5404e0117bddaf12f91c1c86f0e
Zoltan Toth <toth.tozso.zoltan@gmail.com>
2021-06-28 00:25:10 +0200

    C# LINQ - Part 1: Enumerables - The Foundation of LINQ

--- Linq course/Program.cs
+++ Linq course/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Linq_course
-{
-    class Program
+// C# 5 feature: namespace, main is not needed.
+var result = GenerateNumbers(10)
+    .Where(n =>
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+        return n % 2 == 0;
+    })
+    .Select( n =>
+    {
+        return n * 3;
+    });
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+// This method is not called until the foreach above wants to get its result: "pull based approach"
+// It generates a value and returns it immedately. Later it continues from here.
+IEnumerable<int> GenerateNumbers(int maxValue)
+{
+    for (int i = 0; i <= maxValue; i++)
+    {
+        yield return i;
+    }    
 }
