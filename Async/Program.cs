33c494bca7ea9f3861e6af458241426a297ad1a3
Zoltan Toth <toth.tozso.zoltan@gmail.com>
2021-12-21 23:16:41 +0100

    C# Async Programming - Part 2: Intro in Task, async, and await

---
+++ Async/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+#region Sync
+//var lines = File.ReadAllLines("TextFile1.txt");
+
+//foreach (var line in lines)
+//{
+//    Console.WriteLine(line);
+//}
+#endregion
+
+#region Async but blocking way (wrong)
+//var fileReadTask = File.ReadAllLinesAsync("TextFile1.txt");
+
+//fileReadTask.Wait(); // Blocks the thread. It makes synchronous
+//var lines = fileReadTask.Result; // Blocks the thread. It makes synchronous
+#endregion
+
+#region Async old way
+//File.ReadAllLinesAsync("TextFile1.txt")
+//    .ContinueWith(t =>
+//    {
+//        if (t.IsFaulted)
+//        {
+//            Console.Error.WriteLine(t.Exception);            
+//        }
+//        // It runs when the Task is completed!
+//        foreach (var line in t.Result)
+//        {
+//            Console.WriteLine(line);
+//        }
+//    });
+
+// Console.ReadKey();
+#endregion
+
+#region Async new way
+//async Task ReadFile()
+//{
+//    var lines = await File.ReadAllLinesAsync("TextFile1.txt");
+
+//    foreach (var line in lines)
+//    {
+//        Console.WriteLine(line);
+//    }
+//}
+
+//await ReadFile();
+
+async Task<int> GetDataFromNetworkAsync()
+{
+    // Simulate network call
+    await Task.Delay(150);
+    var result = 42;
+    return result;
+}
+
+var networkResult = await GetDataFromNetworkAsync();
+
+Func<Task<int>> GetDataFromNetworkViaLambda = async () =>
+{
+    // Simulate network call
+    await Task.Delay(150);
+    var result = 42;
+    return result;
+};
+
+
+#endregion
