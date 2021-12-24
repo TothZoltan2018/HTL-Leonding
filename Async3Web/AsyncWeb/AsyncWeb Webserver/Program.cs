a4634fdbd0948267d2a3e30b0231461eb7f02fed
Zoltan Toth <toth.tozso.zoltan@gmail.com>
2021-12-24 23:05:55 +0100

    C# Async Programming - Part 3:  Very simple webserver created.

--- Async3Web/AsyncWeb/AsyncWeb Webserver/Program.cs
+++ Async3Web/AsyncWeb/AsyncWeb Webserver/Program.cs
@@ -1,26 +1,34 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace AsyncWeb_Webserver
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+// Creates a WebHost with default settings
+// When an HHTP request comes in, it answers with "Hello World!"
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.Configure(app =>
+        {
+            app.Run(async context =>
+            {
+                // Simulate BAD access of e.g. a DB.
+                //Thread.Sleep(TimeSpan.FromSeconds(5));
+                //Task.Delay(TimeSpan.FromSeconds(5)).Wait(); // As bad as the above line
+
+                // Simulate GOOD access of e.g.a DB.
+                await Task.Delay(TimeSpan.FromSeconds(10));
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+    })
+    .Build().Run();
