6bd964ef84d57aea222e6c3fe8ece2103f59a1c3
Zoltan Toth <toth.tozso.zoltan@gmail.com>
2022-01-21 22:47:13 +0100

    C# ASP.NET Core 5 with Entity Framework 5 - The Fundamentals (Part 1)

---
+++ VideoGameManager/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoGameManager
+{
+    // Selecting from IIS to VisoGameServer ensures that the app will run as a Console app --: We have more control
+    // The Console app fires up a web server
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
